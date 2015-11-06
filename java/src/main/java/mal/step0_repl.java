78f4af88024411c4bc40eec49d9a7301e3387c59
EspressoDan <blackmer.daniel@gmail.com>
2015-11-05 21:03:13 -0800

    Step 0 Complete, everything seems to be working

--- java/src/main/java/mal/step0_repl.java
+++ java/src/main/java/mal/step0_repl.java
@@ -1,48 +1,36 @@
 package mal;
 
-import java.io.IOException;
-
-import mal.readline;
+import java.util.Scanner;
 
 public class step0_repl {
-    // read
-    public static String READ(String str) {
-        return str;
+
+    public static String READ(String input) {
+        return input;
     }
 
-    // eval
-    public static String EVAL(String ast, String env) {
-        return ast;
+    public static String EVAL(String input) {
+        return input;
     }
 
-    // print
-    public static String PRINT(String exp) {
-        return exp;
+    public static String PRINT(String input) {
+        return input;
     }
 
-    // repl
-    public static String RE(String env, String str) {
-        return EVAL(READ(str), env);
+    public static String repl(String input) {
+        return PRINT(EVAL(READ(input)));
     }
 
     public static void main(String[] args) {
-        String prompt = "user> ";
-
-        if (args.length > 0 && args[0].equals("--raw")) {
-            readline.mode = readline.Mode.JAVA;
-        }
+        String input;
+        Scanner scan = new Scanner(System.in);
         while (true) {
-            String line;
-            try {
-                line = readline.readline(prompt);
-                if (line == null) { continue; }
-            } catch (readline.EOFException e) {
-                break;
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-                break;
+            System.out.print("user> ");
+            input = scan.nextLine();
+            if (input.equals("\n") || input.equals("\n\r")) {
+                return;
+            } else {
+                System.out.println(repl(input));
             }
-            System.out.println(PRINT(RE(null, line)));
         }
     }
 }
